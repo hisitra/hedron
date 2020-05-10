f9521b2c3a1bf52c4810a16584adcd55de9e542b
hisitra <shivanshbox@gmail.com>
2020-05-10 17:00:56 +0530

    Basic iotranslator complete

--- src/iotranslator/request.go
+++ src/iotranslator/request.go
@@ -1,6 +1,11 @@
 package iotranslator
 
-import "time"
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"github.com/hisitra/hedron/src/configs"
+	"time"
+)
 
 type Request struct {
 	ID    string `json:"ID"`
@@ -12,6 +17,56 @@ type Request struct {
 	ExpiresAt time.Time `json:"expiresAt"`
 }
 
-func (r *Request) ToJSON() ([]byte, error) {
-	return nil, nil
+func NewRequest(data []byte, mode string) (*Request, *Response) {
+	newReq := &Request{}
+	err := json.Unmarshal(data, newReq)
+	if err != nil {
+		return nil, BadRequestResponse("Failed to Unmarshal")
+	}
+	newReq.Mode = mode
+
+	if resp := newReq.validate(); !resp.IsSuccessful() {
+		return nil, resp
+	}
+
+	newReq.ID = uuid.New().String()
+	newReq.ArrivedAt = time.Now()
+	newReq.ExpiresAt = newReq.ArrivedAt.Add(
+		time.Duration(configs.Server.RequestTimeout) * time.Second)
+
+	return newReq, SuccessResponse("")
+}
+
+func UnmarshalRequest(data []byte) (*Request, *Response) {
+	req := &Request{}
+	err := json.Unmarshal(data, req)
+	if err != nil {
+		return nil, BadRequestResponse("Failed to unmarshal")
+	}
+
+	if resp := req.validate(); !resp.IsSuccessful() {
+		return nil, resp
+	}
+
+	return req, SuccessResponse("")
+}
+
+func (r *Request) validate() *Response {
+	// TODO: Validate key and value using Regex.
+	if r.Key == "" {
+		return BadRequestResponse("Empty key not allowed in Request.")
+	}
+	if r.Mode != "C" && r.Mode != "R" && r.Mode != "U" && r.Mode != "D" {
+		return BadRequestResponse("Invalid Request Type: "+r.Mode)
+	}
+	return SuccessResponse("")
+}
+
+func (r *Request) Marshal() ([]byte, *Response) {
+	reqJSON, err := json.Marshal(r)
+	if err != nil {
+		return nil, InternalServerResponse("")
+	}
+
+	return reqJSON, SuccessResponse("")
 }
--- src/iotranslator/response.go
+++ src/iotranslator/response.go
@@ -1,8 +1,44 @@
 package iotranslator
 
 type Response struct {
+	Code    uint32 `json:"code"`
+	Message string `json:"message"`
+	Data    string `json:"data"`
 }
 
-func (r *Response) ToJSON() ([]byte, error) {
+func NewResponse(code uint32, message string, data string) *Response{
+	return &Response{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func SuccessResponse(message string) *Response {
+	if message == "" {
+		message = "Success"
+	}
+	return NewResponse(200, message, "{}")
+}
+
+func BadRequestResponse(message string) *Response {
+	if message == "" {
+		message = "Bad Request"
+	}
+	return NewResponse(400, message, "{}")
+}
+
+func InternalServerResponse(message string) *Response {
+	if message == "" {
+		message = "Internal Server Error"
+	}
+	return NewResponse(500, message, "{}")
+}
+
+func (r *Response) IsSuccessful() bool {
+	return r.Code >= 200 && r.Code < 300
+}
+
+func (r *Response) Marshal() ([]byte, error) {
 	return nil, nil
 }
